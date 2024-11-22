f57cc07f56678b086560b0dbae3eb33cb4d5ae8b
codeming <hmk2000015@gmail.com>
2024-11-22 19:59:19 +0900

    결제 메인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { SamplePage } from './components/Modal';
 import { useModal } from './hook/useModal';
+import PaymentMain from './pages/PaymentMain';
 
 function App() {
   const { setModal } = useModal();
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="max-w-screen-md">
-        <div>결제 모듈 미션 Init</div>
-        <button onClick={() => setModal(<SamplePage />)}>모달 열기</button>
+      <div className="max-w-screen-md text-center">
+        <div>결제 모듈 미션</div>
+        <button
+          className="bg-mint p-2 text-white mt-5"
+          onClick={() => setModal(<PaymentMain />)}
+        >
+          모달 열기
+        </button>
       </div>
     </div>
   );
